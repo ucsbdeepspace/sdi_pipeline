224aae12e2d6b2f2f8ec97df12d452f535aead04
Varun Iyer <varun_iyer@posteo.net>
2020-07-23 15:05:57 -0700

    Added skimage blob options to extractoin

--- sources/extract.py
+++ sources/extract.py
@@ -6,14 +6,16 @@ History:
 """
 import os
 import sep
+from skimage.feature import blob_log
 from ..common import to_np
 
 
-def extract(residual_s, thresh=None):
+def extract(residual_s, thresh=None, method="sep"):
     """
     Uses sep to find sources on a residual image(s)
     Arguments:
         residuals -- image of residuals from hotpants or a list of images
+        method    -- algorithm to use, sep or skimage
     Returns:
         A list of Source objects representing the location and various metrics
             of detected variable sources
@@ -28,6 +30,12 @@ def extract(residual_s, thresh=None):
         r_np = to_np(r)
         if thresh is None:
             # from astroalign’s settings
-            bkg = sep.Background(r_np)
-        sources.append(sep.extract(r_np - bkg.back(), bkg.globalrms * 3.0, segmentation_map=True))
+            if method=="sep":
+                print("sep")
+                r_np = r_np.byteswap().newbyteorder()
+                bkg = sep.Background(r_np)
+                sources.append(sep.extract(r_np - bkg.back(), bkg.globalrms * 3.0, segmentation_map=False)) # compare overall rms
+            if method=="skimage":
+                print("skimage")
+                sources.append(blob_log(r_np, min_sigma=1, max_sigma=20, num_sigma=10, threshold=0.2))
     return sources if isinstance(residuals, list) else sources[0]
