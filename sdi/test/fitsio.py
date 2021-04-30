90543ca8f926f6a9049dbf69f7710a67d0ad83b3
benfogiel <bfogiel@ucsb.edu>
2021-04-30 14:13:05 -0700

    Tests for Read and Write functions are ready for review

--- sdi/test/fitsio.py
+++ sdi/test/fitsio.py
@@ -4,6 +4,7 @@ import click
 from click.testing import CliRunner
 from astropy.io import fits
 import sdi
+import glob
 
 class TestRead(unittest.TestCase):
 
@@ -31,6 +32,27 @@ class TestRead(unittest.TestCase):
         runner = CliRunner()
         result = runner.invoke(sdi._read_cmd)
         # FIXME figure out how to do click right
+
+class TestWrite(unittest.TestCase):
+
+    def setUp(self):
+        self.h = sdi.read(os.path.join(os.path.dirname(__file__), "fixtures/science"))
+        self.output = sdi.write(self.h, os.path.join(os.path.dirname(__file__), "fixtures/TestDir"), "{number}.fits")
+        self.paths = glob.glob("{}/*.fits*".format(os.path.join(os.path.dirname(__file__), "fixtures/TestDir")))
+
+    def tearDown(self):
+        for hdul in self.h:
+            hdul.close()
+        for p in self.paths:
+            os.remove(p)
+
+    def test_type(self):
+        for o in self.output:
+            self.assertIsInstance(o, fits.HDUList, "Did not read type fits.HDUList")
+
+    def test_dir(self):
+        self.assertEqual(len(self.paths), 10, "Did not write ten HDULs to directory {self.paths}")
+    
 
 if __name__ == "__main__":
     unittest.main()
