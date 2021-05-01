74e32993d1b2f084c03789ebd6bd9c2f181ea06e
Tyler Pruitt <tylerpruitt@umail.ucsb.edu>
2021-05-01 03:27:31 -0700

    Add files via upload

--- sdi/snr.py
+++ sdi/snr.py
@@ -1,7 +1,7 @@
 """
-snr calculates the signal-to-noise ratio of a fits file
 Standalone version without click implementation
 Code originally by Yael Brynjegard-Bialik 2020-2021
+Updated to return modified hduls instead of best SNR hdul by Tyler Pruitt May 1, 2021
 """
 
 
@@ -14,35 +14,50 @@ from photutils import Background2D, DAOStarFinder, detect_threshold, detect_sour
 import matplotlib.pyplot as plt
 
 def snr(hduls, name="SCI"):
-	best = hduls[0]
-	best_snr = 0
-	for hdul in hduls:
-		data = hdul[name].data
-		shape = data.shape
-		# identify background rms
-		boxsize=(shape)
-		bkg = Background2D(data, boxsize)
-		bkg_mean_rms = np.mean(bkg.background_rms)
-
-		# subtract bkg from image
-		new_data = data - bkg.background
-
-		# set threshold and detect sources, threshold 5*std above background
-		threshold = detect_threshold(data=new_data, nsigma=5.0, background=0.0)
-		SegmentationImage = detect_sources(data=new_data, threshold=threshold, npixels=10)
-
-		SourceCatalog = source_properties(new_data, SegmentationImage)
-		columns = ['id', 'xcentroid', 'ycentroid', 'source_sum']
-
-		source_max_values = SourceCatalog.max_value
-		avg_source_max_values = np.mean(source_max_values)
-
-		# calculate signal to noise ratio
-		signal = avg_source_max_values
-		noise = bkg_mean_rms
-		SNR = (signal)/(noise)
-		if SNR > best_snr:
-			best = hdul
-			best_snr = SNR
-		hdul[name].header.append(('SNR',SNR,"signal to noise ratio" ))
-	return best
+    """
+    snr() calculates the signal-to-noise ratio (SNR) of a fits file and writes it to the header
+    
+    Parameters
+    ----------
+    hduls : list of hduls
+        a connection of hduls without the SNR 
+    
+    name : string, optional
+        name of the hdul. The default is "SCI".
+    
+    Returns
+    -------
+    hduls : list of hduls
+        a modified connection of hduls with the SNR value written to the FITS header
+    """
+    
+    for hdul in hduls:
+   		data = hdul[name].data
+   		shape = data.shape
+   		
+        # identify background rms
+   		boxsize=(shape)
+   		bkg = Background2D(data, boxsize)
+   		bkg_mean_rms = np.mean(bkg.background_rms)
+   
+   		# subtract bkg from image
+   		new_data = data - bkg.background
+   
+   		# set threshold and detect sources, threshold 5*std above background
+   		threshold = detect_threshold(data=new_data, nsigma=5.0, background=0.0)
+   		SegmentationImage = detect_sources(data=new_data, threshold=threshold, npixels=10)
+   
+   		SourceCatalog = source_properties(new_data, SegmentationImage)
+   		columns = ['id', 'xcentroid', 'ycentroid', 'source_sum']
+   
+   		source_max_values = SourceCatalog.max_value
+   		avg_source_max_values = np.mean(source_max_values)
+   
+   		# calculate signal to noise ratio
+   		signal = avg_source_max_values
+   		noise = bkg_mean_rms
+   		SNR = (signal)/(noise)
+        
+        # write SNR value to the FITS header
+   		hdul[name].header.append(('SNR', SNR, 'signal to noise ratio'))
+    return hduls
