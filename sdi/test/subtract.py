b666c97272e9138f0663fd2d136a778f192cb182
benfogiel <bfogiel@ucsb.edu>
2021-05-16 09:38:19 -0700

    Subtract test class fully operational and ready for review

--- sdi/test/subtract.py
+++ sdi/test/subtract.py
@@ -1,28 +1,34 @@
+"""
+Author: Benjamin Fogiel - bfogiel@ucsb.edu
+Tests the subtract function
+"""
+
 import os
 import unittest
 import click
 from click.testing import CliRunner
 from astropy.io import fits
 import sdi
+import glob
 
 class TestSubtract(unittest.TestCase):
-
-    path = os.path.join(os.path.dirname(__file__), "fixtures/smallData")
     
     @classmethod
     def setUpClass(cls):
-        cls.read = [s for s in sdi.read(cls.path)]
+        # uses one fits file as subtract param to decrease runtime
+        cls.path = glob.glob(os.path.join(os.path.dirname(__file__), "fixtures/science/tfn0m414-kb99-20180831-0290-e91.fits.fz"))
+        cls.read = [fits.open(p) for p in cls.path]
         cls.output = sdi.subtract(cls.read)
 
     @classmethod
     def tearDownClass(cls):
-        for hdul in cls.read:
-            hdul.close()
+        for o in cls.output:
+            o.close()
 
     def test_type(self):
         for o in TestSubtract.output:
-            self.assertIsInstance(o, fits.HDUList, "Did not subtract type fits.HDUList")
-    
+            self.assertIsInstance(o, fits.HDUList, "Did not output type fits.HDUList")
+
     def test_length(self):
         n = 0
         try:
@@ -30,7 +36,21 @@ class TestSubtract(unittest.TestCase):
                 next(TestSubtract.output)
                 n += 1
         except StopIteration: pass
-        self.assertEqual(n, 4084, "Did not subtract 4084 differences")
+        self.assertEqual(n, 2042, "Did not yield 2042 subtractions")
+
+    def test_output(self):
+        # sorts the known true output to be congruent with the current output
+        self.paths = glob.glob("{}/*.fits*".format(os.path.join(os.path.dirname(__file__), "fixtures/subtractData")))
+        self.paths = sorted(self.paths, key = lambda item: int(item[22:len(item)-5]))
+
+        # compares the knwon true output to current output
+        self.true_output = []
+        for p, o in zip(self.paths, TestSubtract.output):
+            self.true_output = fits.open(p)
+            compare = fits.FITSDiff(self.true_output, o)
+            self.assertEqual(compare.identical, True , compare.report(fileobj = None))
+            self.true_output.close()
+
 
 if __name__ == "__main__":
     unittest.main()
