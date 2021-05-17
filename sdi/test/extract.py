e3f133d2cc9b136c285c0502fe567e7d8b0d9184
benfogiel <bfogiel@ucsb.edu>
2021-05-17 11:35:57 -0700

    Completed all test func except for click

--- sdi/test/extract.py
+++ sdi/test/extract.py
@@ -1,9 +1,15 @@
+"""
+Author: Benjamin Fogiel - bfogiel@ucsb.edu
+Tests the extract function
+"""
+
 import os
 import unittest
 import click
 from click.testing import CliRunner
 from astropy.io import fits
 import sdi
+import glob
 
 class TestExtract(unittest.TestCase):
 
@@ -12,24 +18,30 @@ class TestExtract(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.read = [s for s in sdi.read(cls.path)]
-        cls.output = sdi.extract(cls.read)
+        cls.output = list(sdi.extract(cls.read))
+
+        # sorts the known true output to be congruent with the current output
+        cls.paths = glob.glob("{}/*.fits*".format(os.path.join(os.path.dirname(__file__), "fixtures/comparitiveData/extractData")))
+        cls.paths = sorted(cls.paths, key = lambda item: int(item[37:len(item)-5]))
+        cls.true_output = [fits.open(p) for p in cls.paths]
 
     @classmethod
     def tearDownClass(cls):
-        for hdul in cls.read:
-            hdul.close()
+        for r, t in zip(cls.read, cls.true_output):
+            r.close()
+            t.close()
+
+    def test_type(self):
+        for o in TestExtract.output:
+            self.assertIsInstance(o, fits.HDUList, "Output is not of type fits.HDUList")
 
     def test_length(self):
-        self.assertEqual(len(TestExtract.output), 10, "Extract did not return 10 PrimaryHDU files")
+        self.assertEqual(len(TestExtract.output), 10, "Did not extract 10 fits files")
 
-    def test_length(self):
-        n = 0
-        try:
-            while True:
-                next(TestExtract.output)
-                n += 1
-        except StopIteration: pass
-        self.assertEqual(n, 10, "Did not extract 10 fits files")
+    def test_output(self):
+        for t, o in zip(TestExtract.true_output, TestExtract.true_output):
+            compare = fits.FITSDiff(fits.HDUList(t), fits.HDUList(o))
+            self.assertEqual(compare.identical, True, compare.report(fileobj = None))
 
 if __name__ == "__main__":
     unittest.main()
