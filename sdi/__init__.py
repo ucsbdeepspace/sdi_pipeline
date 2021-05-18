a73edfa8a2990ad99122b11dab53a3ba0d7c1594
benfogiel <bfogiel@ucsb.edu>
2021-05-17 18:46:16 -0700

    added functions to enable click use for testing

--- sdi/__init__.py
+++ sdi/__init__.py
@@ -11,7 +11,7 @@ from . import test
 from .fitsio import read_cmd as _read_cmd
 from .fitsio import write_cmd as _write_cmd
 from .align import align_cmd as _align_cmd
-from .combine import combine_cmd as combine_cmd
+from .combine import combine_cmd as _combine_cmd
 from .extract import extract_cmd as _extract_cmd
 from .subtract import subtract_cmd as _subtract_cmd
 from . import test_cmd
