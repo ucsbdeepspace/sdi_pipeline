290156f5b01314c37ca704a755cc3351cf5186d8
benfogiel <bfogiel@ucsb.edu>
2021-05-17 18:22:59 -0700

    added code to enable click testing for all functions

--- sdi/__init__.py
+++ sdi/__init__.py
@@ -10,6 +10,10 @@ from . import test
 # in order to test click commands
 from .fitsio import read_cmd as _read_cmd
 from .fitsio import write_cmd as _write_cmd
+from .align import align_cmd as _align_cmd
+from .combine import combine_cmd as combine_cmd
+from .extract import extract_cmd as _extract_cmd
+from .subtract import subtract_cmd as _subtract_cmd
 from . import test_cmd
 
 import os
