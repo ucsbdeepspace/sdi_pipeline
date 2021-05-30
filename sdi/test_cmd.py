92e8d715f5bcd30c9c869012d5fa9eb0ed1b5d2d
benfogiel <bfogiel@ucsb.edu>
2021-05-29 19:48:40 -0700

    click for testing complete

--- sdi/test_cmd.py
+++ sdi/test_cmd.py
@@ -8,27 +8,16 @@ from . import test
 @cli.operator
 def test_cmd(hduls, name):
     """
-    Runs all tests through unittest.
+    Runs all tests through unittest
+
+    Avaible tests: fitsio (read and write), align, combine, extract, and subtract
+
+    To run all tests, simply don't pass any parameters
     """
-    if name == "align":
-    	test_suite = unittest.TestLoader().loadTestsFromModule(test.align)
-    elif name == "extract":
-    	test_suite = unittest.TestLoader().loadTestsFromModule(test.extract)
-    elif name == "combine":
-    	test_suite = unittest.TestLoader().loadTestsFromModule(test.combine)
-    elif name == "read":
-    	test_suite = unittest.TestLoader().loadTestsFromModule(test.fitsio)
-    elif name == "write":
-    	test_suite = unittest.TestLoader().loadTestsFromModule(test.fitsio)
-    elif name == "subtract":
-    	test_suite = unittest.TestLoader().loadTestsFromModule(test.subtract)
-    elif name == "test":
-    	test_suite = unittest.TestLoader().loadTestsFromModule(test)
+    if name == "test": 
+        test_suite = unittest.TestLoader().loadTestsFromModule(test)
     else:
-    	print("Specify a valid function you'd like to test:")
-    	print("align, extract, combine, read, write, subtract")
-    	print("Note: if no option is provided, it will run all tests")
-    	return hduls;
+        test_suite = unittest.TestLoader().loadTestsFromModule(getattr(test, name))
 
     runner = unittest.TextTestRunner()
     runner.run(test_suite)
