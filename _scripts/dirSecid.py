fc336637f82c168a168fb17c0a1e22b7e28de36a
agthomas-ucsb <agthoas@ucsb.edu>
2021-07-31 16:40:40 -0700

    Renamed getSecid to imgSecid and created dirSecid. dirSecid indexes an entire directory using glob with support for both fz and fits files.

---
+++ _scripts/dirSecid.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import filedialog
+from astropy.io import fits
+from glob import glob
+import sys
+import os
+
+sdi_path = os.environ['HOME'] + "/sdi_pipeline/sdi"
+sys.path.append(sdi_path) #Find a better way @Varun
+from secid import secid
+
+global sci
+#Hides the Tk() window
+root = tk.Tk()
+root.withdraw()
+ 
+#Opens the file explorer for directory selection
+directory = filedialog.askdirectory(initialdir = "/home/alex/smalldata/science_data", title = "Select a Directory")
+
+if directory == ():  #If the user does not select a file, close the script 
+    print("File not selected. Closing script...")
+    quit()
+
+fz_glob = directory + "/*.fz"
+fits_glob = directory + "/*.fits"
+
+filenames = glob(fz_glob) + glob(fits_glob)
+if filenames == []:
+    raise ValueError('No fitz files in the selected directory')
+
+for i,filename in enumerate(filenames):
+    image = fits.open(filename)
+    secid_gen = secid(image, "SCI")
+    #print(secid_gen)
+    output = next(secid_gen)
+    print(f"{filename} is in section {output[0].header['SECID']}")
+    #print(output)
+
