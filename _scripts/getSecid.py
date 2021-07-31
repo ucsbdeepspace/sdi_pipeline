4b0457c57e6d775945e96cbac73afb1c2880ff7b
agthomas-ucsb <agthoas@ucsb.edu>
2021-07-31 16:19:16 -0700

    Updated the script using a workaround to work from the _scripts directory within SDI and modified the message outputted by getSecid.py

--- _scripts/getSecid.py
+++ _scripts/getSecid.py
@@ -1,16 +1,24 @@
 import tkinter as tk
 from tkinter import filedialog
 from astropy.io import fits
-from sdi_pipeline.sdi.secid import secid
+import sys
+import os
+
+sdi_path = os.environ['HOME'] + "/sdi_pipeline/sdi"
+sys.path.append(sdi_path) #Find a better way @Varun
+from secid import secid
+
 global sci
 #Hides the Tk() window
 root = tk.Tk()
 root.withdraw()
-
+ 
 #Opens the file explorer for fits file selection
 path = filedialog.askopenfilename(initialdir = "/home/alex/smalldata/science_data", title = "Select a File", filetypes = (("Compressed Fitz files", "*.fz*"), ("Fitz files", "*.fits"), ("all files", "*.*")))
- 
-#print(path)
+
+if path == ():  #If the user does not select a file, close the script 
+    print("File not selected. Closing script...")
+    quit()
 
 image = fits.open(path)
 
@@ -19,7 +27,7 @@ image = fits.open(path)
 secid_gen = secid(image, "SCI")
 #print(secid_gen)
 output = next(secid_gen)
-print(output[0].header['SECID'])
+print(f"{path} is in section {output[0].header['SECID']}")
 #print(output)
 
 #for value in secid_gen:
--- sdi/secid.py
+++ sdi/secid.py
@@ -1,45 +1,49 @@
-"""
-Convert DEC and RA values to secid for m31
-"""
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+
 RABOUND = [12.44,11.97,11.48,11,10.52,10.04,9.56]    
 _RASCALE = [-1,0,9,18,27,36,45]
 DECBOUND = [42.77,42.45,42.13,41.81,41.49,41.17,40.85,40.53,40.21,39.89]
 _DECSCALE = [-1,9,8,7,6,5,4,3,2,1]
-def secid(hduls, write_ext=0):
+def secid(hduls, read_ext='SCI', write_ext=0):
     """
     secid adds M31 section ID information to the header of the HDU specified in write_ext.
     If an HDUL does not have WCS data or is not in M31, a ValueError will be raised.
     :param hduls: a collection or generator of HDUL to process.
     :param write_ext: an extname or index number of an HDU to write secid information into.
-    """  
-      
-    RA = sci.header['RA']                                                           
-    DEC = sci.header['DEC']                                                         
+    """
+    #print(hduls)
+    primary = hduls[read_ext]
+    #print(primary)
+    RA = primary.header['RA']                                                           
+    DEC = primary.header['DEC']                                                         
     sc = SkyCoord(RA + ' ' + DEC, unit = (u.hourangle,u.deg))                       
-    SCRA = sc.ra*u.deg                                                              
+    SCRA = sc.ra*u.deg                                                             
     SCDEC = sc.dec*u.deg   
-    SECid = -1
     RAid = -1
     DECid = -1
+    #Constraints the SECID to one 'row' based on the RA
     for i in range(len(RABOUND)):
-        if RA > RABOUND[i]:
+        if SCRA.value > RABOUND[i]:
             RAid = _RASCALE[i]
             break
- 
+    
+    #Figures out what SECID you are precisely in using DEC
     for i in range(len(DECBOUND)):
-        if DEC > DECBOUND[i]:
+        if SCDEC.value > DECBOUND[i]:
             DECid = _DECSCALE[i]
             break
+    #Creates the SECid if both the RA and DEC were found within bounds.
     if DECid != -1 and RAid != -1: 
         SECid = DECid + RAid
-
+    else:
+        SECid = -1
+    #print(SECid)
 # if HDUL does not have WCS data OR SECid == -1:
 #    raise ValueError("bad SECid")
 ## should this be one error or one error in each case?
 ##What is WCS data?
-
     hduls[write_ext].header.set('SECID', SECid)
-    print(hduls[write_ext].header['SECID'])
-## This doesn't save!
+    #print(hduls[write_ext].header['SECID'])
 
     yield hduls
