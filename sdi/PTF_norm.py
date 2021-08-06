7cb8f06aaafa391f59b1e49b04cd4a644416da5b
PK <prerana0207@gmail.com>
2021-08-06 03:46:51 -0700

    Changed file to account for different lengths in data inputs. Did this with try, except ValueError.

--- sdi/PTF_norm.py
+++ sdi/PTF_norm.py
@@ -12,12 +12,17 @@ from glob import glob
 import numpy as np
 
 def normalize(flux,ref_flux,ref_mag):
-    return ref_mag-2.5*np.log10(flux/ref_flux)
+    #If ref flux is longer
+    try:
+        return ref_mag[0:len(flux)]-2.5*np.log10(flux/ref_flux[0:len(flux)])
+    #If cat flux is longer
+    except ValueError:
+        return ref_mag-2.5*np.log10(flux[0:len(ref_flux)]/ref_flux)
 
 """
 This function takes in one fits file at a time, and normalizes it with respect to the Gaia reference table magnitudes of stars after transforming the Gaia data color information to the SDSS12 g-filter according to the Gaia documentation available at: https://gea.esac.esa.int/archive/documentation/GDR2/Data_processing/chap_cu5pho/sec_cu5pho_calibr/ssec_cu5pho_PhotTransf.html
 """
-#TODO: find a way to write information back to the XRT table in the hduls. For now it is in CAT
+#TODO: find a way to write information back to the CAT table in the hduls.
 
 def norm(hduls):
     cat_table = hduls['CAT']
@@ -35,12 +40,20 @@ def norm(hduls):
     #transforming reference magnitudes to SDSS12 g
     gaia_mag_transformed = ref_mag-0.13518+0.4625*(g_bp_g-g_rp_g)+0.25171*(g_bp_g-g_rp_g)**2-0.021349*(g_bp_g-g_rp_g)**3
     gaia_mag_err = 0.16497
-
+    
     #transforming reference flux to SDSS12 g    
     #Find c by fitting to our data. It is linear in c and not a very complicated model so we can use np.linalg.lstsq
-    A = np.ones((len(cat_flux),1))
-    A[:, 0] = 10**(-2.5*gaia_mag_transformed)
-    c_best = np.linalg.lstsq(A, cat_flux, rcond=None)[0]
+    #If gaia is shorter
+    try:
+        A = np.ones((len(gaia_mag_transformed),1))
+        A[:, 0] = 10**(-2.5*gaia_mag_transformed)
+        c_best = np.linalg.lstsq(A, cat_flux[0:len(gaia_mag_transformed)], rcond=None)[0]
+    #If cat_flux is shorter
+    except ValueError:
+        A = np.ones((len(cat_flux),1))
+        A[:, 0] = 10**(-2.5*gaia_mag_transformed[0:len(cat_flux)])
+        c_best = np.linalg.lstsq(A, cat_flux, rcond=None)[0]
+
     gaia_flux_transformed = np.dot(A, c_best)
     
     c_err = np.sqrt(np.sum(10**(2.5*gaia_mag_transformed)*gaia_mag_err))
@@ -55,9 +68,14 @@ def norm(hduls):
     gaia_flux_err = np.sqrt((10**(-2.5*gaia_mag_transformed)*c_err)**2+((np.log(10)*c_best*gaia_mag_err)/(2.5*10**(2.5*gaia_mag_transformed)))**2)
     
     #Uncertainty in calculated magnitudes:
-    mag_err = np.sqrt((gaia_mag_err)**2+((-2.5*cat_fluxerr)/(np.log(10)*cat_flux))**2+((2.5*gaia_flux_err)/(np.log(10)*gaia_flux_transformed))**2)
-
+    #If ref flux is longer
+    try:
+        mag_err = np.sqrt((gaia_mag_err)**2+((-2.5*cat_fluxerr)/(np.log(10)*cat_flux))**2+((2.5*gaia_flux_err)/(np.log(10)*gaia_flux_transformed[0:len(cat_flux)]))**2)
+    #If cat flux is longer
+    except ValueError:
+        mag_err = np.sqrt((gaia_mag_err)**2+((-2.5*cat_fluxerr)/(np.log(10)*cat_flux[0:len(ref_mag)]))**2+((2.5*gaia_flux_err)/(np.log(10)*gaia_flux_transformed))**2)
     #write cat_mag and mag_err to the hdul
+    #Have to redo writing method so it writes to CAT 
     out_cols = cat_table.columns
     new_cols = fits.ColDefs([fits.Column(name='mag', format='D',array=cat_mag),fits.Column(name='magerr', format='D',array=mag_err)])
     hdu = fits.BinTableHDU.from_columns(out_cols + new_cols)
