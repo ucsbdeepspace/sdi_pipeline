6aca05725fc3593ca92974ec92bccdeeeda630b0
Kelvin Yip <kelvinyip@ucsb.edu>
2021-10-09 15:48:30 -0700

    Added unit testing for the collate command, which includes fits files collated data.

--- sdi/__init__.py
+++ sdi/__init__.py
@@ -18,6 +18,7 @@ from .align import align_cmd as _align_cmd
 from .combine import combine_cmd as _combine_cmd
 from .extract import extract_cmd as _extract_cmd
 from .subtract import subtract_cmd as _subtract_cmd
+from .collate import collate_cmd as _collate_cmd
 from . import test_cmd
 
 _scidir = os.path.join(os.path.dirname(__file__), "test/fixtures/science")
