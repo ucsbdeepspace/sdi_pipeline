aa0dfb7681f5378313b4be06818e0ffddbc88bb5
Jasper <jxwebb@icloud.com>
2022-02-22 21:19:25 -0800

    implementing new SNR script into align

--- sdi/align.py
+++ sdi/align.py
@@ -3,36 +3,52 @@ align -- this module aligns sets of astronomical data
 HISTORY
     Created/Substantially refactored on 2019-09-01
         Varun Iyer <varun_iyer@ucsb.edu>
+    Refactored SNR section on 2022-2-22
+        Jasper Webb <jlw@ucsb.edu>
 """
 # general imports
-
 import click
 import numpy as np
 from astropy.io.fits import CompImageHDU
 #from sources import Source
 import astroalign
-from _scripts import snr
+from .snr_function import snr 
+#from _scripts import snr
 from . import _cli as cli
 
-def align(hduls, name="SCI", reference=None):
+def align(hduls, name="SCI", ref=None):
     """
     Aligns the source astronomical image(s) to the reference astronomical image
     \b
     :param hduls: list of fitsfiles
-    :return: list of fistfiles with <name> HDU aligned
+    :param name: header containing image data.
+    :param reference: index of desired 
+    :return: list of FITS files with <name> HDU aligned
     """
 
     hduls_list = [hdul for hdul in hduls]
     outputs = []
 
-    if reference is None:
-        reference = snr.snr(hduls_list, name)[name]
-    # click.echo(reference.header["ORIGNAME"])
-    # FIXME log ref name
-    np_ref = reference
+    hduls_list = snr(hduls_list,name) #SNR function appends snr to each hdul's "CAT" table.
+    hduls_list = list(hduls_list) #convert generator to list.
+
+    if ref is None: #No reference index given. we establish reference based on best signal to noise ratio
+
+        reference = hduls_list[0][name] #by default first hdul is reference
+        ref_SNR = hduls_list[0]["CAT"].header["SNR"]
+
+        for hdul in hduls_list: #loops though hdul lists finding the hdul with greatest snr.
+            if hdul["CAT"].header["SNR"] > ref_SNR: #compares SNR value of current hdul to ref
+                ref_SNR = hdul["CAT"].header["SNR"]
+                reference = hdul[name]
+
+    else: #ref index is provided
+        reference = hduls_list[ref][name]
+
     try:
-        np_ref = np_ref.data
+        ref_data = reference.data
     except AttributeError:
+        print("The reference file have attritube: Data") 
         pass
 
     for i,hdul in enumerate(hduls_list):
@@ -42,13 +58,13 @@ def align(hduls, name="SCI", reference=None):
         output = np.array([])
         
         try:
-            output = astroalign.register(np_src, np_ref)[0]
+            output = astroalign.register(np_src,ref_data)[0]
         except ValueError:
             np_src = hdul[name].data.byteswap().newbyteorder()
-            output = astroalign.register(np_src, np_ref)[0]
+            output = astroalign.register(np_src, ref_data)[0]
             pass
 
-        if hasattr(hdul[name], "data"): ##not sure what happens if this if statement fails. previously it would just write empty data. With this set up data would be left alone.
+        if hasattr(hdul[name], "data"): 
             idx = hdul.index_of(name)
             hdul[idx].data = output
             hdul[idx].header['EXTNAME'] = ("ALGN    ")
@@ -61,11 +77,11 @@ def align(hduls, name="SCI", reference=None):
 #TODO use CAT sources if they exist
 
 ## align function wrapper
-def align_cmd(hduls, name="SCI", reference=None):
+def align_cmd(hduls, name="SCI", ref=None):
     """
     Aligns the source astronomical image(s) to the reference astronomical image
     \b
     :param hduls: list of fitsfiles
     :return: list of fistfiles with <name> HDU aligned
     """
-    return align([hduls for hduls in hduls], name, reference)
+    return align([hduls for hduls in hduls], name, ref)
