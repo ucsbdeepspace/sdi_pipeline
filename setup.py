f37d167731722c7b5ba6c619e6ba57e5bfb563fa
KyleLam <kylelam@ucsb.edu>
2022-10-29 18:39:18 -0700

    Test Setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,50 @@
+import os
+import sys
 from setuptools import setup, find_packages
 
-setup(
-    name="sdi-cli",
-    version="0.99",
-    py_modules=["sdi"],
-    # packages=find_packages(include=["openfits"]),
-    include_package_data=True,
-    setup_requires = ['numpy', "setuptools_rust"]
-    install_requires=["click", "astropy", "photutils", "ois", "astroalign", "astroquery", "sklearn"],
-    entry_points="""
-        [console_scripts]
-        sdi=sdi._cli:cli
-    """,
-)
+DESCRIPTION = "Pipeline to Take a Stack of Images and Extract Residuals"
+NAME = "sdi-pipeline"
+AUTHOR = "Kyle Lam"
+AUTHOR_EMAIL = "kylelam@ucsb.edu"
+MAINTAINER = "Kyle Lama"
+MAINTAINER_EMAIL = "kylelam@ucsb.edu"
+DOWNLOAD_URL = "https://github.com/ucsbdeepspace/sdi_pipeline.git"
+
+LICENSE = 'MIT Licence'
+VERSION = '0.99'
+
+install_reqs = ["numpy",
+				"setuptools_rust",
+				"click", 
+				"astropy", 
+				"photutils", 
+				"ois", 
+				"astroalign", 
+				"astroquery", 
+				"sklearn",
+				]
+setup_reqs = ["numpy", 
+				"setuptools_rust"]
+
+setup(name = NAME,
+      version = VERSION,
+      description = DESCRIPTION,
+	  setup_requires = setup_reqs,
+      install_requires = install_reqs,
+      author = AUTHOR,
+      author_email = AUTHOR_EMAIL,
+      maintainer = MAINTAINER,
+      maintainer_email = MAINTAINER_EMAIL,
+      download_url = DOWNLOAD_URL,
+      license = LICENSE,
+      packages = find_packages(),
+      include_package_data = True,
+    #   classifiers = [
+    #     'Development Status :: 4 - Beta',
+    #     'Environment :: Console',
+    #     'Intended Audience :: Science/Research',
+    #     'Natural Language :: English',
+    #     'Programming Language :: Python :: 3.7',
+    #     'Programming Language :: Python :: 3 :: Only',
+    #     'Topic :: Scientific/Engineering :: Astronomy'],
+     )
