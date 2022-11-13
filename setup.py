786e2b1e6193a812305b6de64ab68da50e164635
aleclwang <99686350+aleclwang@users.noreply.github.com>
2022-11-13 13:09:35 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     py_modules=["sdi"],
     # packages=find_packages(include=["openfits"]),
     include_package_data=True,
-    install_requires=["click", "astropy", "photutils", "ois", "astroalign", "astroquery", "sklearn"],
+    install_requires=["sfft", "click", "astropy", "photutils", "ois", "astroalign", "astroquery", "sklearn"],
     entry_points="""
         [console_scripts]
         sdi=sdi._cli:cli
