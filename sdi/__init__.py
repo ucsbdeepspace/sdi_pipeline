189b9fb4927ee95a5c48890dd09282f55bf794d1
Scientin <hunterjex@gmail.com>
2022-11-21 17:56:32 -0800

    Updated __init__
    Updated __init__.py to accomodate middle_night.py

--- sdi/__init__.py
+++ sdi/__init__.py
@@ -5,6 +5,7 @@ from .combine import combine
 #from .display import display
 from .fitsio import read, write
 from .subtract import subtract
+from .middle_night import middle_night
 from .extract import extract
 from .ref import ref
 from .secid import secid
@@ -18,6 +19,7 @@ from .fitsio import write_cmd as _write_cmd
 from .align import align_cmd as _align_cmd
 from .combine import combine_cmd as _combine_cmd
 from .extract import extract_cmd as _extract_cmd
+from .middle_night import middle_night_cmd as _middle_night_cmd
 from .subtract import subtract_cmd as _subtract_cmd
 from .collate import collate_cmd as _collate_cmd
 #from .snr_function import snr_cmd as _snr_cmd #New
