80dd9b47c339b93c89cf9a333e8a76c5fd060912
agthomas-ucsb <agthoas@ucsb.edu>
2024-02-13 18:29:50 -0800

    Fixed the file structure issues that were created from merging and readded setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import subprocess
+import sys
+
+'''
+try:
+    import numpy
+except ModuleNotFoundError:
+    import sys
+    sys.exit("numpy not found, tripp requires numpy for installation.\n Please try '$pip3 install numpy'.")
+
+try:
+    import setuptools_rust
+except ModuleNotFoundError:
+    import sys
+    sys.exit("setuptools_rust not found, tripp requires setuptools_rust for installation.\n Please try '$pip3 install setuptools_rust'.")
+'''
+
+subprocess.run(sys.executable + " -m pip install --upgrade pip setuptools==56.0.0 setuptools_rust numpy", shell=True)
+
+setup(
+    name="tripp-cli",
+    version="0.99",
+    py_modules=["tripp"],
+    # packages=find_packages(include=["openfits"]),
+    include_package_data=True,
+    install_requires=["click", "astropy", "photutils", "ois", "astroalign", "astroquery", "scikit-learn"],
+    entry_points="""
+        [console_scripts]
+        tripp=tripp._cli:cli
+    """,
+)
